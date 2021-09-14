b04c9b94e963e878216330387f59219b387aa686
BrandonDY <deyoung.brandon@gmail.com>
2021-09-14 19:07:55 -0400

    Redid canvas logic

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from tkinter import *
+import tkinter as tk
+
+class CHESSBOARD:
+    black = "#000000"
+    white = "#ffffff"
+    highlight = "#77db69"
+    rows = 8
+    cols = 8
+    tileDim = 64
+
+    def __init__(self, parent):
+        canvas_width = self.cols * self.tileDim
+        canvas_height = self.rows * self.tileDim
+        self.canvas = tk.Canvas(parent, width=canvas_width+100, height=canvas_height+200)
+        self.canvas.pack(padx=8, pady=8)
+        
+
 
 def main():
-    root = Tk()
+    root = tk.Tk()
     root.title('Chess Game')
+    chessboard = CHESSBOARD(root)
     icon = PhotoImage(file="./icons/mainIcon.png")
     root.iconphoto(False, icon)
-    root.geometry("800x800")
     root.resizable(False, False)
     root.mainloop()
 
