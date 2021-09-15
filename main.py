851ca00fec212a3e90fe9e5721bc0e7c2f5f5ca7
BrandonDY <deyoung.brandon@gmail.com>
2021-09-14 20:46:06 -0400

    Board drawn

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ from tkinter import *
 import tkinter as tk
 
 class CHESSBOARD:
-    black = "#000000"
-    white = "#ffffff"
+    color1 = "#adede4"
+    color2 = "#e8ebea"
     highlight = "#77db69"
     rows = 8
-    cols = 8
-    tileDim = 64
+    columns = 8
+    dim_square = 64
 
     def __init__(self, parent):
-        canvas_width = self.cols * self.tileDim
-        canvas_height = self.rows * self.tileDim
-        self.canvas = tk.Canvas(parent, width=canvas_width+100, height=canvas_height+200)
+        canvas_width = self.columns * self.dim_square + 100
+        canvas_height = self.rows * self.dim_square + 200
+        self.canvas = tk.Canvas(parent, width=canvas_width, height=canvas_height)
         self.canvas.pack(padx=8, pady=8)
-        
+        self.draw_board()
 
+    def draw_board(self):
+        color = self.color2
+        for row in range(self.rows):
+            color = self.color1 if color == self.color2 else self.color2
+            for col in range(self.columns):
+                x1 = (col * self.dim_square)+2
+                y1 = ((7 - row) * self.dim_square) + 100
+                x2 = x1 + self.dim_square
+                y2 = y1 + self.dim_square
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color,
+                                                 tags="area")
+                color = self.color1 if color == self.color2 else self.color2
 
 def main():
     root = tk.Tk()
