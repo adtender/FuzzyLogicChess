fb9c8e4a523bb1ed7a9bdcf0b3df9656c5015f58
BrandonDY <deyoung.brandon@gmail.com>
2021-09-15 00:05:08 -0400

    Added cursor tracking

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
 import tkinter as tk
+import math
 
 class CHESSBOARD:
     color1 = "#706677"
     color2 = "#ccb7ae"
-    highlight = "#77db69"
+    highlight = "#eefaac"
     rows = 8
     columns = 8
     dim_square = 64
@@ -15,6 +16,7 @@ class CHESSBOARD:
         self.canvas = tk.Canvas(parent, width=canvas_width, height=canvas_height)
         self.canvas.pack(padx=8, pady=8)
         self.draw_board()
+        #self.canvas.bind("<Button-1>", self.square_clicked)
 
     def draw_board(self):
         intCheck = 0
@@ -22,7 +24,7 @@ class CHESSBOARD:
             intCheck += 1
             for col in range(self.columns):
                 x1 = (col * self.dim_square) + 2
-                y1 = ((row) * self.dim_square) + 100
+                y1 = (row * self.dim_square) + 100
                 x2 = x1 + self.dim_square
                 y2 = y1 + self.dim_square
                 self.canvas.create_rectangle(x1, y1, x2, y2,
@@ -30,12 +32,25 @@ class CHESSBOARD:
                                                 tags="area")
                 intCheck += 1
 
-
     def checkerboard_color(self, intCheck):
         if intCheck % 2 == 0:
             return self.color1
         else:
             return self.color2
+
+    #def locToCoords(self, loc):
+        
+
+def motion(event):
+    x, y = event.x - 2, event.y - 100
+    over = math.ceil(x/64)+64 
+    down = abs(math.ceil(y/64) - 9)
+    overChar = chr(over)
+    if x > 0 and x <= 512 and y > 0 and y <= 512:
+        loc = str(overChar) + str(down)
+        #print(over - 64,down)
+        print(loc)
+        return over - 64, down, loc
 
 def main():
     root = tk.Tk()
@@ -44,6 +59,7 @@ def main():
     icon = PhotoImage(file="./icons/mainIcon.png")
     root.iconphoto(False, icon)
     root.resizable(False, False)
+    root.bind('<Motion>', motion)
     root.mainloop()
 
 if __name__ == "__main__":
